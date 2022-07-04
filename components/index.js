0f34d57ac7b728bc64160a122d7b6fcd663a8a20
CoderAnchel <alex21fd@gmail.com>
2022-07-04 22:34:49 +0200

    first commit

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./button";
+
+export {
+    HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton
+}
